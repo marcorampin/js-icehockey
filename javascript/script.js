a4fc4aadd0ffe8148d53902bcfbac1aa62d3a05b
Marco Rampin <rampin.marco.17@gmail.com>
2024-10-09 15:53:17 +0200

    general improvement and availability to move horizontally the paddle

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,10 @@
+//Need to make a class for field lines (only rectangles across the field)
+//Maybe include a multiplayer (wasd vs arrows)
+//Find a way to make the text centered (scores)
+//Find if it's possible to use two circles and simulate the collision with them instead of paddles
+//Update the canva size when the window is resized (from old project, maybe Haiku or quote generator)
+//Check if other variables are needed
+
 //Screen size
 const winWidth = window.innerWidth * 0.9;
 const winHeight = window.innerHeight * 0.9;
@@ -33,14 +40,16 @@ var player;
 var opponent;
 var cursors;
 var playerScoreText;
-var aiScoreText;
+var opponentScoreText;
 var playerScore = 0;
-var aiScore = 0;
-const init_vel = width_canva / 7;
+var opponentScore = 0;
+const init_vel = width_canva / 5;
 const bounce_eff = 1.1;
 const paddle_xside = width_canva / 15;
 const paddle_width = 20;
 const paddle_height = height_canva / 5;
+var divisor;
+const ball_radius = 10;
 
 class Paddle {
 	//gives the selected characteristics
@@ -69,8 +78,21 @@ class Paddle {
 		this.paddle.body.setVelocityY(init_vel * 1.5);
 	}
 	
-	stop() {
+	stop(axis) {
+	if (axis == 'y') {
 		this.paddle.body.setVelocityY(0);
+		}
+	else if (axis == 'x') {
+			this.paddle.body.setVelocityX(0);
+		}
+	}
+	
+	moveRight() {
+		this.paddle.body.setVelocityX(init_vel * 1.5);
+	}
+	
+	moveLeft() {
+		this.paddle.body.setVelocityX(-init_vel * 1.5);
 	}
 }
 
@@ -86,7 +108,7 @@ class Ball {
 	this.ball = scene.add.circle(this.pos_x, this.pos_y, this.radius, this.color)
 	scene.physics.add.existing(this.ball);
 	this.ball.body.setBounce(bounce_eff, bounce_eff); //X and Y bounce efficiency
-	this.ball.body.setCollideWorldBounds(true, bounce_eff, bounce_eff); //Ball bounces of walls
+	this.ball.body.setCollideWorldBounds(true, 1, 1); //Ball bounces of walls
 	}
 	
 	randomNum() {
@@ -117,9 +139,7 @@ function preload(){
 }
 
 //Create function to initialize the game
-function create(){
-
-	
+function create(){	
 	//Add paddles (rectangles)
 	player = new Paddle(paddle_xside, height_canva / 2, paddle_width, paddle_height, 0xffffff);
 	player.set_physics(this);
@@ -127,8 +147,12 @@ function create(){
 	opponent.set_physics(this);
 	
 	//Create ball
-	sphere = new Ball(width_canva / 2, height_canva / 2, 10, 0xffffff)
+	sphere = new Ball(width_canva / 2, height_canva / 2, ball_radius, 0xffffff)
 	sphere.set_physics(this); //Makes ball a physics object
+	
+	//Add division in the middle of the field
+	divisor = new Paddle(width_canva / 2, height_canva / 2, 5, height_canva, 0xffffff)
+	divisor.set_physics(this);
 	
 	//Give ball an initial velocity
 	sphere.setVelocity();
@@ -141,8 +165,8 @@ function create(){
 	cursors = this.input.keyboard.createCursorKeys();
 	
 	//Create score display
-	playerScoreText = this.add.text(320, 20, 'Player: 0', { fontSize: '32px', fill: '#fff' });
-	aiScoreText = this.add.text(430, 20, 'AI: 0', {fontSize: '32px', fill: '#fff'});
+	playerScoreText = this.add.text(width_canva * 0.20, 20, 'Player: 0', { fontSize: '30px', fill: '#fff' });
+	opponentScoreText = this.add.text(width_canva * 0.60, 20, 'Opponent: 0', {fontSize: '30px', fill: '#fff'});
 }
 
 //Update function for game logic
@@ -155,32 +179,40 @@ function update() {
 		player.moveDown();
 	}
 	else {
-		player.stop();
+		player.stop('y');
 	}
 	
+	if (cursors.right.isDown) {
+		player.moveRight();
+	}
+	else if (cursors.left.isDown) {
+		player.moveLeft();
+	}
+	else {
+		player.stop('x');
+	}
 	//Simple AI: Move the AI paddle towards the ball
-	if ((sphere.ball.y + 10 > opponent.paddle.y + opponent.height / 4) && (sphere.ball.body.velocity.x > 0)) {
+	if (sphere.ball.y + ball_radius > opponent.paddle.y + opponent.height / 4) {
 		opponent.moveDown();
 	}
-	else if ((sphere.ball.y - 10 < opponent.paddle.y - opponent.height / 4) && (sphere.ball.body.velocity.x  > 0)) {
+	else if (sphere.ball.y - ball_radius < opponent.paddle.y - opponent.height / 4) {
 		opponent.moveUp();
 	}
 	else {
-		opponent.stop();
+		opponent.stop('y');
 	}
 	
-	//Restrict player paddle movement to within game bounds
-	//player.paddle.y = Phaser.Math.Clamp(player.paddle.y, player.height / 2, config.height - player.height / 2);
-	//Ensures AI paddle to stay within bounds
-	//opponent.paddle.y = Phaser.Math.Clamp(opponent.paddle.y, opponent.height / 2, config.height - opponent.height / 2);
+	//Adds bounds to the divisor
+	player.paddle.x = Phaser.Math.Clamp(player.paddle.x, player.width / 2, width_canva / 2 - player.width / 2);
+	opponent.paddle.x = Phaser.Math.Clamp(opponent.paddle.x, width_canva / 2 + opponent.width / 2, width_canva - opponent.width / 2);	
 	
 	//Check if ball goes off left or right side to score points
-	if (sphere.ball.x <= 0 + 10) {
+	if (sphere.ball.x <= 0 + ball_radius * 2) {
 		sphere.resetBall();
-		aiScore++;
-		aiScoreText.setText('AI: ' + aiScore);
+		opponentScore++;
+		opponentScoreText.setText('Opponent: ' + opponentScore);
 	}
-	else if (sphere.ball.x >= width_canva - 10) {
+	else if (sphere.ball.x >= width_canva - (ball_radius * 2)) {
 		sphere.resetBall();
 		playerScore++;
 		playerScoreText.setText('Player: ' + playerScore);
