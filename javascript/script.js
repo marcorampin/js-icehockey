77f5614246f7e267c56472059109058930d60539
Marco Rampin <rampin.marco.17@gmail.com>
2024-10-10 12:21:49 +0200

    Automated page refresher

--- javascript/script.js
+++ javascript/script.js
@@ -2,12 +2,28 @@
 //Maybe include a multiplayer (wasd vs arrows)
 //Find a way to make the text centered (scores)
 //Find if it's possible to use two circles and simulate the collision with them instead of paddles
-//Update the canva size when the window is resized (from old project, maybe Haiku or quote generator)
 //Check if other variables are needed
+//Find a way to increase also the vertical speed of the ball (right now, with high speed the vertical component is almost null)
 
 //Screen size
-const winWidth = window.innerWidth * 0.9;
-const winHeight = window.innerHeight * 0.9;
+let winWidth = window.innerWidth * 0.9;
+let winHeight = window.innerHeight * 0.9;
+
+var ball;
+var player;
+var opponent;
+var cursors;
+var playerScoreText;
+var opponentScoreText;
+var playerScore = 0;
+var opponentScore = 0;
+let init_vel = winWidth / 5;
+const bounce_eff = 1.1;
+let paddle_xside = winWidth / 15;
+let paddle_width = 20;
+let paddle_height = winHeight / 5;
+var divisor;
+const ball_radius = 10;
 
 // Game Configuration
 var config = {
@@ -31,25 +47,6 @@ var config = {
 
 //Create the Phaser Game instance
 var game = new Phaser.Game(config)
-
-
-const width_canva = winWidth;
-const height_canva = winHeight;
-var ball;
-var player;
-var opponent;
-var cursors;
-var playerScoreText;
-var opponentScoreText;
-var playerScore = 0;
-var opponentScore = 0;
-const init_vel = width_canva / 5;
-const bounce_eff = 1.1;
-const paddle_xside = width_canva / 15;
-const paddle_width = 20;
-const paddle_height = height_canva / 5;
-var divisor;
-const ball_radius = 10;
 
 class Paddle {
 	//gives the selected characteristics
@@ -94,6 +91,13 @@ class Paddle {
 	moveLeft() {
 		this.paddle.body.setVelocityX(-init_vel * 1.5);
 	}
+	
+	updateResize() {
+		this.pos_x = pos_x;
+		this.pos_y = pos_y;
+		this.width = width;
+		this.height = height;
+	}
 }
 
 class Ball {	
@@ -109,6 +113,7 @@ class Ball {
 	scene.physics.add.existing(this.ball);
 	this.ball.body.setBounce(bounce_eff, bounce_eff); //X and Y bounce efficiency
 	this.ball.body.setCollideWorldBounds(true, 1, 1); //Ball bounces of walls
+	this.ball.body.maxSpeed = this.pos_x * 1.5;
 	}
 	
 	randomNum() {
@@ -141,17 +146,17 @@ function preload(){
 //Create function to initialize the game
 function create(){	
 	//Add paddles (rectangles)
-	player = new Paddle(paddle_xside, height_canva / 2, paddle_width, paddle_height, 0xffffff);
+	player = new Paddle(paddle_xside, winHeight / 2, paddle_width, paddle_height, 0xffffff);
 	player.set_physics(this);
-	opponent = new Paddle(width_canva - paddle_xside, height_canva / 2, paddle_width, paddle_height, 0xffffff);
+	opponent = new Paddle(winWidth - paddle_xside, winHeight / 2, paddle_width, paddle_height, 0xffffff);
 	opponent.set_physics(this);
 	
 	//Create ball
-	sphere = new Ball(width_canva / 2, height_canva / 2, ball_radius, 0xffffff)
+	sphere = new Ball(winWidth / 2, winHeight / 2, ball_radius, 0xffffff)
 	sphere.set_physics(this); //Makes ball a physics object
 	
 	//Add division in the middle of the field
-	divisor = new Paddle(width_canva / 2, height_canva / 2, 5, height_canva, 0xffffff)
+	divisor = new Paddle(winWidth / 2, winHeight / 2, 5, winHeight, 0xffffff)
 	divisor.set_physics(this);
 	
 	//Give ball an initial velocity
@@ -165,8 +170,8 @@ function create(){
 	cursors = this.input.keyboard.createCursorKeys();
 	
 	//Create score display
-	playerScoreText = this.add.text(width_canva * 0.20, 20, 'Player: 0', { fontSize: '30px', fill: '#fff' });
-	opponentScoreText = this.add.text(width_canva * 0.60, 20, 'Opponent: 0', {fontSize: '30px', fill: '#fff'});
+	playerScoreText = this.add.text(winWidth * 0.20, 20, 'Player: 0', { fontSize: '30px', fill: '#fff' });
+	opponentScoreText = this.add.text(winWidth * 0.60, 20, 'Opponent: 0', {fontSize: '30px', fill: '#fff'});
 }
 
 //Update function for game logic
@@ -202,9 +207,9 @@ function update() {
 		opponent.stop('y');
 	}
 	
-	//Adds bounds to the divisor
-	player.paddle.x = Phaser.Math.Clamp(player.paddle.x, player.width / 2, width_canva / 2 - player.width / 2);
-	opponent.paddle.x = Phaser.Math.Clamp(opponent.paddle.x, width_canva / 2 + opponent.width / 2, width_canva - opponent.width / 2);	
+	//Adds bounds for paddles to not go over the middle field
+	player.paddle.x = Phaser.Math.Clamp(player.paddle.x, player.width / 2, winWidth / 2 - player.width / 2);
+	opponent.paddle.x = Phaser.Math.Clamp(opponent.paddle.x, winWidth / 2 + opponent.width / 2, winWidth - opponent.width / 2);	
 	
 	//Check if ball goes off left or right side to score points
 	if (sphere.ball.x <= 0 + ball_radius * 2) {
@@ -212,9 +217,16 @@ function update() {
 		opponentScore++;
 		opponentScoreText.setText('Opponent: ' + opponentScore);
 	}
-	else if (sphere.ball.x >= width_canva - (ball_radius * 2)) {
+	else if (sphere.ball.x >= winWidth - (ball_radius * 2)) {
 		sphere.resetBall();
 		playerScore++;
 		playerScoreText.setText('Player: ' + playerScore);
 	}
 }
+
+//Function to get window size
+function updateWindowSize() {
+    location.reload();
+}
+
+window.addEventListener('resize', updateWindowSize);
